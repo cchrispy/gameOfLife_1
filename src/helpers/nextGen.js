33e557e2149508aeb7ceb7cf6c681c0274839483
Chris Chiang <cchiang1992@gmail.com>
2016-11-19 18:49:53 -0800

    getNeighbors function

--- src/helpers/nextGen.js
+++ src/helpers/nextGen.js
@@ -1,13 +1,30 @@
 const nextGen = cells => {
-  // returns an object with the cells that are alive in the next generation
+  // returns an object with the cells that need to be toggled
 }
-
-
-const getNeighbors = key => {
-  // returns an array a cell's neighbors
-}
-
 
 const liveNeighbors = key => {
   // returns a count of the number of live neighbors a cell has
+}
+
+const getNeighbors = key => {
+  // returns an array a cell's neighbors
+
+  if (key === 1) { // top left
+    return [2, 61, 62];
+  } else if (key === 60) { // top right
+    return [59, 119, 120];
+  } else if (key === 3541) { // bottom left
+    return [3481, 3482, 3542];
+  } else if (key === 3600) { // bottom right
+    return [3539, 3540, 3599];
+  } else if (key < 60) { // top row
+    return [key - 1, key + 1, key + 59, key + 60, key + 61];
+  } else if (key > 3540) { // bottom row
+    return [key - 1, key + 1, key - 61, key - 60, key - 59];
+  } else if (key % 60 === 0) { // right column
+    return [key - 1, key - 61, key - 60, key + 59, key + 60];
+  } else if ((key - 1) % 60 === 0) { // left column
+    reutrn [key + 1, key - 60, key - 59, key + 60, key + 61];
+  }
+  
 }
