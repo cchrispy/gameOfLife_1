4281dcb540535d05817bc835fd66aa6a6af59465
Chris Chiang <cchiang1992@gmail.com>
2016-11-19 18:38:25 -0800

    Create helper function to figure out which cells to toggle

---
+++ src/helpers/nextGen.js
@@ -0,0 +1,13 @@
+const nextGen = cells => {
+  // returns an object with the cells that are alive in the next generation
+}
+
+
+const getNeighbors = key => {
+  // returns an array a cell's neighbors
+}
+
+
+const liveNeighbors = key => {
+  // returns a count of the number of live neighbors a cell has
+}
