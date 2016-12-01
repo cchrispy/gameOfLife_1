248ded47721558c3e81bc2afd5116c7b6d4f0e31
Chris Chiang <cchiang1992@gmail.com>
2016-11-30 19:20:59 -0800

    Remove default cells from reducer and put it in Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,10 +2,37 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Board from './Board.jsx';
 
+import toggle from '../actions/toggleAction.js';
+
 
 class Main extends Component {
   constructor(props) {
     super(props);
+  }
+
+  componentWillMount() {
+    var temp = { // starting board, hardcoded cells
+      1300: true, 
+      1301: true, 
+      1240: true, 
+      1241: true, 
+      1242: true, 
+      1182: true, 
+      1183: true,
+
+      1400: true,
+      1401: true,
+      1460: true,
+      1461: true,
+      1462: true,
+
+      1900: true,
+      1958: true,
+      1960: true,
+      2019: true,
+      2020: true
+    }
+    this.props.toggle(temp);
   }
 
   render() {
@@ -22,4 +49,10 @@ const mapStateToProps = state => ({
   store: state
 })
 
-export default connect(mapStateToProps)(Main);
+const mapDispatchToProps = dispatch => ({
+  toggle: cells => {
+    dispatch(toggle(cells))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Main);
--- src/reducers/cellState.js
+++ src/reducers/cellState.js
@@ -1,27 +1,4 @@
-var temp = { // starting board, hardcoded cells
-  // 1300: true, 
-  // 1301: true, 
-  // 1240: true, 
-  // 1241: true, 
-  // 1242: true, 
-  // 1182: true, 
-  // 1183: true,
-
-  // 1400: true,
-  // 1401: true,
-  // 1460: true,
-  // 1461: true,
-  // 1462: true,
-
-  1900: true,
-  1958: true,
-  1960: true,
-  2019: true,
-  2020: true
-}
-
-
-const cellState = (state = temp, action) => {
+const cellState = (state = {}, action) => {
   switch (action.type) {
     case 'TOGGLE':
       return Object.assign({}, state, action.cells);
