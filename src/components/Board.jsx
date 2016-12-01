9a01162f65f295ac53bf04a3b1c4be12eecd4043
Chris Chiang <cchiang1992@gmail.com>
2016-11-30 18:23:48 -0800

    Each render is changing store twice, as seen from double console.logs from store

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -15,21 +15,15 @@ class Board extends Component {
   componentWillMount() {
     // var cells = [/*1700, 1699, 1701, 1702, 1703, 1640, 1760, */
     //              1300, 1301, 1240, 1241, 1242, 1182, 1183];
-    // cells.forEach(cell => {
-    //   this.props.toggle(cell);
-    // });
     this.iterate = setTimeout(() => {
-      console.log(nextGen(this.props.cellState));
-      // nextGen(this.props.cellState).forEach(cell => {
-      //   this.props.toggle(cell);
-      // })
+      // console.log(nextGen(this.props.cellState));
       this.props.toggle(nextGen(this.props.cellState));
       this.props.iterate('next');
     }, 1000)
   }
 
   componentWillUnmount() {
-    clearInterval(this.iterate);
+    // clearInterval(this.iterate);
   }
 
   render() {
