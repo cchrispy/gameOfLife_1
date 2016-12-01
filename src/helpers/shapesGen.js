05fcc244c199d9847c2713c9256b16096dda0a2d
Chris Chiang <cchiang1992@gmail.com>
2016-11-30 19:29:55 -0800

    List a few shapes

--- src/helpers/shapesGen.js
+++ src/helpers/shapesGen.js
@@ -1,5 +1,27 @@
-var shapes = {
+var shapes = { // shapes will be returned as objects with proper cells
+  glider: () => {
 
+  },
+
+  lightweightSpaceship: () => {
+
+  },
+
+  pulsar: () => {
+
+  },
+
+  beacon: () => {
+
+  },
+
+  blinker: () => {
+
+  },
+
+  gliderGun: () => {
+    // https://en.wikipedia.org/wiki/Gun_(cellular_automaton)
+  }
 }
 
 export default shapes;
