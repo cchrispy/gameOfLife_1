9a165a91ada7c2b4e16b1290d2cc52e042d41cbc
Chris Chiang <cchiang1992@gmail.com>
2016-11-30 19:09:48 -0800

    Glitch in nextGen to not kill some live cells

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -13,8 +13,6 @@ class Board extends Component {
   }
 
   componentWillMount() {
-    // var cells = [/*1700, 1699, 1701, 1702, 1703, 1640, 1760, */
-    //              1300, 1301, 1240, 1241, 1242, 1182, 1183];
     this.iterate = setTimeout(() => {
       this.props.toggle(nextGen(this.props.cellState));
       this.props.iterate('next');
