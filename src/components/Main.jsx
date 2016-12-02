499eafc31202110c7b191e4f03c7f5c8a5d76d0d
Chris Chiang <cchiang1992@gmail.com>
2016-12-01 18:18:24 -0800

    Generate random cells to be on by default

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,25 +12,28 @@ class Main extends Component {
 
   componentWillMount() {
     var temp = { // starting board, hardcoded cells
-      1300: true, 
-      1301: true, 
-      1240: true, 
-      1241: true, 
-      1242: true, 
-      1182: true, 
-      1183: true,
+      // 1300: true, 
+      // 1301: true, 
+      // 1240: true, 
+      // 1241: true, 
+      // 1242: true, 
+      // 1182: true, 
+      // 1183: true,
 
-      1400: true,
-      1401: true,
-      1460: true,
-      1461: true,
-      1462: true,
+      // 1400: true,
+      // 1401: true,
+      // 1460: true,
+      // 1461: true,
+      // 1462: true,
 
-      1900: true,
-      1958: true,
-      1960: true,
-      2019: true,
-      2020: true
+      // 1900: true,
+      // 1958: true,
+      // 1960: true,
+      // 2019: true,
+      // 2020: true
+    }
+    for (var i = 0; i < 400; i++) {
+      temp[Math.floor(Math.random()*3400) + 100] = true;
     }
     this.props.toggle(temp);
   }
