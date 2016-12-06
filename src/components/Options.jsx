095089099aed35d7ead45db9f3e9ef5a4ca32a49
Chris Chiang <cchiang1992@gmail.com>
2016-12-05 17:29:54 -0800

    Option component with cellcount and glidercount inputs

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -26,31 +26,39 @@ class Options extends Component {
     })
   }
 
+  submit(e) {
+    e.preventDefault();
+    console.log('submitted: ', e.target);
+  }
+
   render() {
     return (
       <div>
-        <div className='form-group'>
-          <label htmlFor='cellCount'>Number of cells: { this.state.cellCountInput } </label>
-          <input type='range' 
-                 min='10'
-                 max='1000'
-                 value={ this.state.cellCountInput }
-                 step='1'
-                 className='form-control' 
-                 onChange={ this.adjustCellCount.bind(this) } 
-                 id='cellCount' ></input>
-        </div>
-        <div className='form-group'>
-          <label htmlFor='gliderCount'>Number of gliders: { this.state.gliderCountInput } </label>
-          <input type='range'
-                 min='0'
-                 max='10'
-                 value={ this.state.gliderCountInput }
-                 step='1'
-                 className='form-control'
-                 onChange={ this.adjustGliderCount.bind(this) }
-                 id='gliderCount' ></input>
-        </div>
+        <form onSubmit={ this.submit.bind(this) } >
+          <div className='form-group'>
+            <label htmlFor='cellCount'>Number of cells: { this.state.cellCountInput } </label>
+            <input type='range' 
+                   min='10'
+                   max='1000'
+                   value={ this.state.cellCountInput }
+                   step='1'
+                   className='form-control' 
+                   onChange={ this.adjustCellCount.bind(this) } 
+                   id='cellCount' ></input>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='gliderCount'>Number of gliders: { this.state.gliderCountInput } </label>
+            <input type='range'
+                   min='0'
+                   max='10'
+                   value={ this.state.gliderCountInput }
+                   step='1'
+                   className='form-control'
+                   onChange={ this.adjustGliderCount.bind(this) }
+                   id='gliderCount' ></input>
+          </div>
+          <button type='submit' className='btn btn-primary'>Submit</button>
+        </form>
       </div>
     )
   }
