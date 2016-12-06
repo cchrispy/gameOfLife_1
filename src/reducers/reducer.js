a75e0adf71559bd485caee14febeb8dce8230035
Chris Chiang <cchiang1992@gmail.com>
2016-12-05 18:55:36 -0800

    Added stats object to the redux store tracks cell count, glider count, blinker count

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import iteration from './iteration.js';
 import cellState from './cellState.js';
+import stats from './stats.js';
 
 export default combineReducers({
   iteration,
-  cellState
+  cellState,
+  stats
 });
---
+++ src/reducers/stats.js
@@ -0,0 +1,20 @@
+const stats = (state = {}, action) {
+  switch (action.type) {
+    case 'CELL_COUNT':
+      return Object.assign({}, state, {
+        cellCount: action.cellCount
+      });
+    case 'GLIDER_COUNT':
+      return Object.assign({}, state, {
+        gliderCount: action.gliderCount
+      });
+    case 'BLINKER_COUNT':
+      return Object.assign({}, state, {
+        blinkerCount: action.blinkerCount
+      });
+    default:
+      return state;
+  }
+}
+
+export default stats;
