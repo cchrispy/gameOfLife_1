643a03400cf0634af7ac549ef451311314587ea9
Chris Chiang <cchiang1992@gmail.com>
2016-12-05 17:19:34 -0800

    Input range from 10-1000 cells

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -4,18 +4,30 @@ import { connect } from 'react-redux';
 class Options extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      cellCountInput: 400
+    }
+  }
+
+  adjustCount(e) {
+    e.preventDefault();
+    this.setState({
+      cellCountInput: e.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <div className='form-group'>
-          <label htmlFor='cellCount'>Number of cells</label>
+          <label htmlFor='cellCount'>Number of cells: { this.state.cellCountInput } </label>
           <input type='range' 
                  min='10'
-                 max='3000'
-                 value='400'
+                 max='1000'
+                 value={ this.state.cellCountInput }
+                 step='1'
                  className='form-control' 
+                 onChange={ this.adjustCount.bind(this) } 
                  id='cellCount' ></input>
         </div>
         <div className='form-group'>
