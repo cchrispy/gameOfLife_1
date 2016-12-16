367b2ed98ead5ffbe9c7b8b01489cc803a6f0a73
Chris Chiang <cchiang1992@gmail.com>
2016-12-15 16:31:15 -0800

    Add bootstrap tooltip; add hoverable rules

--- src/components/Rules.jsx
+++ src/components/Rules.jsx
@@ -4,6 +4,12 @@ class Rules extends Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.rules = {
+      underPopulation: 'Live cells with fewer than 2 live neighbor die',
+      stability: 'Live cells with 2 or 3 live neighbor lives on',
+      overPopulation: 'Live cells with more than 3 live neighbor die',
+      reproduction: 'Dead cells with 3 live neighbors becomes alive'
+    }
   }
 
   render() {
@@ -26,10 +32,10 @@ class Rules extends Component {
       <div id='rules' className='dropup pull-right'>
         <button className='btn btn-info' data-toggle='dropdown' >Rules</button>
         <ul className='dropdown-menu dropdown-menu-right' role='menu'>
-          <li><a href='#'>Under-population</a></li>
-          <li><a href='#'>Stability</a></li>
-          <li><a href='#'>Over-population</a></li>
-          <li><a href='#'>Reproduction</a></li>
+          <li><a href='#' data-toggle='tooltip' title={ this.rules.underPopulation } >Under-population</a></li>
+          <li><a href='#' data-toggle='tooltip' title={ this.rules.stability } >Stability</a></li>
+          <li><a href='#' data-toggle='tooltip' title={ this.rules.overPopulation } >Over-population</a></li>
+          <li><a href='#' data-toggle='tooltip' title={ this.rules.reproduction } >Reproduction</a></li>
         </ul>
       </div>
     )
